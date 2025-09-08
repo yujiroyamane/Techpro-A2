e7ab93a67a94d24114bfdd059d42be428dd4892f
yujiroyamane <yamaneyujiro7@gmail.com>
2025-09-08 14:30:12 +0000

    Seatwork

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+package myproject6;
+
+public class MyClass {
+
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8,} };
+    System.out.println("myNumbers[1][2]");
+  }
+}
 
